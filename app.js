936c40ba40007126d575ecd571a45033948debda
Andres <aizarra@yahoo.com>
2022-11-10 15:26:50 +0100

    need more CSS work - still not clear on how the partials work

--- app.js
+++ app.js
@@ -14,12 +14,39 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
 
-app.get('/', (req, res) => {
+app.get('/home', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beers => {
+      console.log('Beers from the database: ', beers);
+      res.render('beers', { beers });
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      // your magic happens here
+      res.render('random-beer', { responseFromAPI });
+    })
+    .catch(error => console.log(error));
+});
+app.get('/beers/beerId', (req, res) => {
+  punkAPI
+    .getBeer(req.params.beerId)
+    .then((responseFromAPI) => {
+      res.render('beers/beer-details.hbs', { beers: responseFromApi });
+    })
+    .catch(err => console.log(err));
+});
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
